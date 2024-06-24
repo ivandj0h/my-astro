3e30753a9946c0f1ff7f2da9398bb7b920957b20
ivandjoh <ivandi.djoh@gmail.com>
2024-06-24 14:25:22 +0800

    major updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,25 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import { remarkReadingTime } from "./src/remarkReadingTime";
+import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
+import svelte from "@astrojs/svelte";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap(), svelte(), sentry(), spotlightjs()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime]
+  },
+  output: "server",
+  // adapter: vercel(),
+  adapter: netlify(),
+  site: "https://rhythm.nation",
+  server: {
+    port: 3000
+  }
 });
